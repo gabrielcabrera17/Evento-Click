60147597be1a50ab0997745eb31e24f90a54d2c6
gabrielcabrera17 <gabrielbaez023@gmail.com>
2024-04-18 23:09:27 -0400

    Evento click

---
+++ script.js
@@ -0,0 +1,9 @@
+function cambiarTexto(elementoACambiar){
+    elementoACambiar.innerHTML = "Logout";
+}
+function removerBoton(elementoARemover){
+    elementoARemover.remove();
+}
+function alertaMeGusta(elementoMeGusta){
+    alert("Ninja was liked");
+}
